d6841eab4528bb031178675d5ff755151fa48273
Nirgigo <titouan@dupuisland.net>
2025-12-03 19:57:05 +0100

    InitProject

--- TheBindingOfTaxi/scripts/roomdata.gd
+++ TheBindingOfTaxi/scripts/roomdata.gd
@@ -1,4 +1,18 @@
-extends Node2D
+@tool
+class_name RoomData extends Node2D
+
+@export_tool_button("Save Room")
+var button = save_room
+
+@export var roomName : String = "new_room"
+@export var dirPath : String = "TheBindingOfTaxi/ressources/roomdata/"
+var lastSaveName : String = ""
+var lastSaveTry : bool = false
+
+@export var RoadLayer : TileMapLayer
+@export var WallLayer : TileMapLayer
+@export var EmptyLayer : TileMapLayer
+
 @export var room_size : Vector2i = Vector2i.ONE
 @export var currentPos : Vector2i = Vector2i(0, 0)
 #@export_flags("WEST", "NORTH", "SOUTH", "EAST") var DirectionTest : int = 0;
@@ -13,3 +27,31 @@ var exitValue : int = 0;
 
 func _ready() -> void:
 	pass
+
+
+func save_room() -> void :
+	var dirPath = "res://" + dirPath
+	
+	var dir = DirAccess.open(dirPath)
+	if (dir == null) :
+		DirAccess.make_dir_recursive_absolute(dirPath)
+	
+	dirPath = dirPath + roomName
+	dir = DirAccess.open(dirPath)
+	if (dir != null) :
+		if (roomName == lastSaveName && lastSaveTry) :
+			DirAccess.remove_absolute(dirPath)
+			dir = DirAccess.make_dir_recursive_absolute(dirPath)
+		else :
+			print("! WARNING ! : '" + roomName + "' already exist in this directory. If you want to overwrite this directory, press 'Save Room' button again")
+			lastSaveName = roomName
+			lastSaveTry = true
+			return
+	else :
+		dir = DirAccess.make_dir_recursive_absolute(dirPath)
+	
+	lastSaveName = ""
+	lastSaveTry = false
+	
+	print("Save Room Test : ", roomName)
+	return
