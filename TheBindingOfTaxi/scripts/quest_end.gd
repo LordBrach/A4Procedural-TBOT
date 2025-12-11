30c45ed4920b332f70114fdd2d1f71c1ebbe8dba
Nirgigo <titouan@dupuisland.net>
2025-12-11 18:01:15 +0100

    [ADD] QuestEnd Animation

--- TheBindingOfTaxi/scripts/quest_end.gd
+++ TheBindingOfTaxi/scripts/quest_end.gd
@@ -13,25 +13,36 @@ var isAddedToPlayer : bool = false
 @export var zoneSprite1 : Sprite2D
 @export var zoneSprite2 : Sprite2D
 
-@export var startAnimAlpha : float = 0.8
+@export var startAnimAlpha : float = 1
 @export var animDuration : float = 2
+@export var fullSize : float = 32
 
 var time : float = 0
 
 func _ready() -> void:
 	zoneSprite1.modulate = zoneColor
 	zoneSprite2.modulate = zoneColor
-	
-	#if(!isAddedToPlayer) : 
-		#Player.Instance.SavedExits.append(self)
-		#isAddedToPlayer = true
-	 #await get_tree().create_timer(1.0).timeout
+	zoneSprite1.modulate.a = 0
+	zoneSprite2.modulate.a = 0
 
 func _process(delta: float) -> void:
 	if (isActive) :
 		time += delta
 		
-		var sprite1timer = time / animDuration
+		var sprite1timer : float = fmod(time, animDuration) / animDuration
+		var sprite2timer : float = fmod(time - (animDuration / 2), animDuration) / animDuration
+		
+		zoneSprite1.scale = get_from_fullsize(sprite1timer)
+		zoneSprite2.scale = get_from_fullsize(sprite2timer)
+		zoneSprite1.modulate.a = startAnimAlpha - (startAnimAlpha * sprite1timer)
+		zoneSprite2.modulate.a = startAnimAlpha - (startAnimAlpha * sprite2timer)
+
+func get_from_fullsize(a_size : float) -> Vector2 :
+	var result : Vector2
+	
+	result.x = (hitbox.shape.get_rect().size.x / fullSize) * a_size
+	result.y = (hitbox.shape.get_rect().size.y / fullSize) * a_size
+	return result
 
 func on_collect() -> void:
 	super()
@@ -48,6 +59,8 @@ func _on_body_entered(body:Node2D) -> void:
 		if(Player.Instance.CustomerList.has(LinkedClientId)):
 			Player.Instance.complete_quest(LinkedClientId)
 			isActive = false
+			zoneSprite1.modulate.a = 0
+			zoneSprite2.modulate.a = 0
 			#super(body)
 	else:
 		pass
